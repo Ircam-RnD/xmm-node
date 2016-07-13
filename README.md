a455781b432a9f066755a1dd522d26304b310713
Joseph <joseph.larralde@gmail.com>
2016-07-13 13:10:02 +0200

    first commit

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
-# XMM native NodeJS addon
+# XMM native addon for NodeJS
+
+Wraps [XMM](https://github.com/julesfrancoise/xmm) library into a NodeJS module
 
 ## Prerequisites :
+- install latest NodeJS version
+- install node-gyp : `npm install -g node-gyp`
+- install dependencies listed in `package.json` : `npm install`
 
-Donwload and install bla bla bla
+## Build :
+`node-gyp configure build`
+
+## Test :
+`node xmm.js`
+
+## Note
+this module is a work in progress, use at your own risk
--- build/Release/.deps/Release/obj.target/xmm/xmm/src/core/common/xmmAttribute.o.d
+++
@@ -1,6 +0,0 @@
-cmd_Release/obj.target/xmm/xmm/src/core/common/xmmAttribute.o := c++ '-DNODE_GYP_MODULE_NAME=xmm' '-D_DARWIN_USE_64_BIT_INODE=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DBUILDING_NODE_EXTENSION' -I/Users/larralde/.node-gyp/4.4.4/include/node -I/Users/larralde/.node-gyp/4.4.4/src -I/Users/larralde/.node-gyp/4.4.4/deps/uv/include -I/Users/larralde/.node-gyp/4.4.4/deps/v8/include -I../node_modules/nan  -Os -gdwarf-2 -mmacosx-version-min=10.7 -arch x86_64 -Wall -Wendif-labels -W -Wno-unused-parameter -w -std=gnu++0x -fno-rtti -fno-threadsafe-statics -std=c++11 -stdlib=libc++ -MMD -MF ./Release/.deps/Release/obj.target/xmm/xmm/src/core/common/xmmAttribute.o.d.raw   -c -o Release/obj.target/xmm/xmm/src/core/common/xmmAttribute.o ../xmm/src/core/common/xmmAttribute.cpp
-Release/obj.target/xmm/xmm/src/core/common/xmmAttribute.o: \
-  ../xmm/src/core/common/xmmAttribute.cpp \
-  ../xmm/src/core/common/xmmAttribute.hpp
-../xmm/src/core/common/xmmAttribute.cpp:
-../xmm/src/core/common/xmmAttribute.hpp:
