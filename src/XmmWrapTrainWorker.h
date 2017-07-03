53588b12d8c52bfbe16f0117d1cb3dc24c7a272e
Joseph <joseph.larralde@gmail.com>
2017-07-03 17:09:03 +0100

    another attempt to fix model still training issues

--- src/XmmWrapTrainWorker.h
+++ src/XmmWrapTrainWorker.h
@@ -62,7 +62,9 @@ public:
 
     // this should be safe to do this, as HandleOKCallback
     // is called in the event loop
+    std::cout << "before assignment" << std::endl;
     original = tool;
+    std::cout << "after assignment" << std::endl;
     // call reset so the model is ready to filter
     original.reset();
     callback->Call(2, results);
